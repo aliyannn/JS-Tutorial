423acb8bbf1091e02e28e49e051fac2bf44d22a0
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2023-11-26 19:39:45 +0000

    variables investigation JS

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountID = 12345;
+let accountEmail = "aliyan@gmail.com";
+var accountPassword = "112233";
+accountCity = "Lahore";
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
+
+// accountID = 1223;
+
+accountEmail = "aliyangohar@gmail.com"
+accountPassword = "123123";
+accountCity = "Islamabad"
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
