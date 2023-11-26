950dd53a39028d2b6576a0a7a7af4746293a9c97
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2023-11-26 19:43:44 +0000

    variables investigation JS

--- basics/variables.js
+++ basics/variables.js
@@ -2,13 +2,19 @@ const accountID = 12345;
 let accountEmail = "aliyan@gmail.com";
 var accountPassword = "112233";
 accountCity = "Lahore";
+let accountState;
 
-console.table([accountID, accountEmail, accountPassword, accountCity]);
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
 
 // accountID = 1223;
 
 accountEmail = "aliyangohar@gmail.com"
 accountPassword = "123123";
 accountCity = "Islamabad"
+accountState = "Punjab"
 
-console.table([accountID, accountEmail, accountPassword, accountCity]);
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
+
+/* Prefer not to use VAR because of issue in block and functional variable */
+// console.table is used to print all values in bulk
+// It is not a good practice to assign a variable without defining its data type
