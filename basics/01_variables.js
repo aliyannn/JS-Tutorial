4e64657acdbc12dda7d965132debf0ae1f026c72
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2023-11-27 22:38:50 +0000

    File name changed

---
+++ basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID = 12345;
+let accountEmail = "aliyan@gmail.com";
+var accountPassword = "112233";
+accountCity = "Lahore";
+let accountState;
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
+
+// accountID = 1223;
+
+accountEmail = "aliyangohar@gmail.com"
+accountPassword = "123123";
+accountCity = "Islamabad"
+accountState = "Punjab"
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
+
+/* Prefer not to use VAR because of issue in block and functional variable */
+// console.table is used to print all values in bulk
+// It is not a good practice to assign a variable without defining its data type
+// Const value cannot be changed by the compiler
