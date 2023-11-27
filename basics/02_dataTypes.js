9c45a925e534422e5f6e14333ae6ae83eca6d9fc
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2023-11-27 23:05:20 +0000

    Data Types learning in JS

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict" // treat all JS code as newer version of JS
+
+// alert(3+3) // we are using Node.JS, not browser.
+
+// number => 2 to power 46
+// bigInt // for a very big number
+// string => ""
+// boolean => true/false
+// null => standalone value // empty value
+// undefined => value not assigned, yet!
+// symbol => unique
