60bd8b74b406bd9254550e3be7e8057bc834f1ea
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2023-12-08 17:02:20 +0000

    Operations and conversions JS

---
+++ basics/04_operations.js
@@ -0,0 +1,22 @@
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// // console.log(2**2); // 2 to the power of 2
+
+// let str1 = "hello ";
+// let str2 = "Aliyan";
+
+// console.log(str1 + str2); // concatinating 2 strings
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2"); // Since the JS engine reads the line one by one (horizontally), so that the first two are numbers, 
+// /* it will add them and then it will see that the next number is a string, hence it will concatenate the previous number with the string, resulting in 32. */
+// console.log("1" + 2 + 2); // This is the vice versa of the above case, the first number is a string so it will concatenate the string with the next number and 
+// /* it will keep on adding the next numbers to the string, resulting in 122. */
+
+// console.log(+true); // Here, + is used as a unary operator which is also equivalent to Number(true), which converts boolean to number
+// console.log(true+true); // So here, it will add both the numbers
+// console.log(+""); // Unary operator will convert string into number, since the string is empty it will result in 0
+// console.log(+"1"); // unary operator will convert string into number 
