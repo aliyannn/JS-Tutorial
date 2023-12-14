3cab35c500e55dc28d6cc19fbb4eeca651d07136
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2023-12-14 22:37:28 +0000

    Stack and heap memory

--- basics/datatypes_summary.js
+++ basics/datatypes_summary.js
@@ -5,11 +5,26 @@
 const id = Symbol('123');
 const anotherId = Symbol('123');
 
-console.log(id);
-console.log(anotherId);
+// console.log(id);
+// console.log(anotherId);
 
-console.log(id === anotherId)
+// console.log(id === anotherId)
 
 const bigNumber = 13516366321386216586n;
 
-console.log(typeof bigNumber)
+// console.log(typeof bigNumber)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitve)
+
+// When stack memory is used, you'll get a copy of the variable declared
+// When heap memory is used, you'll get a reference of the original value
+
+let myYoutubename = "aliyangohar"
+
+let anotherName = myYoutubename
+anotherName = 'allygohar'
+
+console.log(myYoutubename)
+console.log(anotherName)
