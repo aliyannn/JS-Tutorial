f32d38e5a433d159ee62ee49444980fb19da6d19
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2023-12-14 22:56:30 +0000

    Stack and Heap memory, new extensions

--- basics/datatypes_summary.js
+++ basics/datatypes_summary.js
@@ -2,8 +2,8 @@
 
 // Reference (Non-Primitive) => Array, Objects, Functions.
 
-const id = Symbol('123');
-const anotherId = Symbol('123');
+const id = Symbol("123");
+const anotherId = Symbol("123");
 
 // console.log(id);
 // console.log(anotherId);
@@ -21,10 +21,26 @@ const bigNumber = 13516366321386216586n;
 // When stack memory is used, you'll get a copy of the variable declared
 // When heap memory is used, you'll get a reference of the original value
 
-let myYoutubename = "aliyangohar"
+let myYoutubename = "aliyangohar";
 
-let anotherName = myYoutubename
-anotherName = 'allygohar'
+let anotherName = myYoutubename;
+anotherName = "allygohar";
 
-console.log(myYoutubename)
-console.log(anotherName)
+console.log(myYoutubename);
+console.log(anotherName);
+
+// In the above code, since the type is primitive, hence stack is in use and we get a copy of the variable
+// i.e. myYoutubename. A copy is given to the other variable, i.e. anotherName.
+// Hence, myYoutubename value will not be changed.
+
+let userOne = {
+    email: 'aliyangohar@gmail.com',
+    id: 123
+}
+
+let userTwo = userOne
+
+userTwo.id = 321
+
+console.log(userOne.id)
+console.log(userTwo.id);
