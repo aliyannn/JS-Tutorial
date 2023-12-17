a87f048165bb5c31c23414b2455c6703c2def6ef
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2023-12-17 02:29:53 +0000

    strings in JS

---
+++ basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = 'Aliyan';
+const repoCount = 50;
+
+
+// console.log(name + repoCount + ' Value');
+
+console.log(`${name} has ${repoCount} repositories`);
+
+// How to declare a string
+const gameName = new String('AliyanAG');
