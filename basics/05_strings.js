a8248ac479f7eaeaf4bc68b717294c860a062d04
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2023-12-23 17:41:36 +0000

    JS strings methods

--- basics/05_strings.js
+++ basics/05_strings.js
@@ -8,3 +8,25 @@ console.log(`${name} has ${repoCount} repositories`);
 
 // How to declare a string
 const gameName = new String('AliyanAG');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__)
+
+console.log(gameName.length);
+
+// toUpperCase without parenthesis () will be used to store a reference in it. For example;
+// let myString = 'Hello World';
+// let myFunc = myString.toUpperCase;
+// console.log(myFunc());
+
+// toUpperCase with paranthesis () is a method in JS to convert a string to uppercase.
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('i'));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-7, 4);
+console.log(anotherString);
