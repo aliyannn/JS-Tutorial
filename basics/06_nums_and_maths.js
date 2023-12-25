5f51f84cc0de50fb838bdab120dbf8eecfe8dbe0
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2023-12-25 22:38:24 +0000

    numbers and maths JS

---
+++ basics/06_nums_and_maths.js
@@ -0,0 +1,11 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString());
+
+// console.log(typeof(score));
+// console.log(typeof(balance));
+// console.log(typeof(balance.toString()));
