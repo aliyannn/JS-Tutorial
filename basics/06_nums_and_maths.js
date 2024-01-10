63e439af2a7d0da005d887fb13e112183707e099
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2024-01-10 23:57:57 +0000

    maths and numbers

--- basics/06_nums_and_maths.js
+++ basics/06_nums_and_maths.js
@@ -14,20 +14,25 @@ console.log(balance.toString()); // Converts number to string.
 
 // console.log(balance.length);
 console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+console.log(balance.toFixed(2));
 
 const otherNumber = 123.8966;
 console.log(otherNumber.toPrecision(3));
 
 const hundreds = 1000000;
-console.log(hundreds.toLocaleString('en-IN'));
-
+console.log(hundreds.toLocaleString("en-IN"));
 
 // +++++++++++++++++++++++++ Maths +++++++++++++++++++++++++
 
-console.log(Math)
+console.log(Math);
 console.log(Math.abs(-4)); // Changes negative values to positive.
 console.log(Math.round(4.5)); // Rounds decimal values to higher if greater than equal to five, else lower value.
 console.log(Math.ceil(4.1)); // Returns greater value even if not greater than or equal to five.
 console.log(Math.floor(4.9)); // Returns lower value even if greater than or equal to five.
+console.log(Math.min(4, 3, 6, 8)); // Returns the minimum value
+console.log(Math.max(4, 3, 6, 8)); // Returns the maximum value
 
+console.log(Math.random());
+console.log(Math.random() + 1);
+console.log(Math.random() * 10);
+console.log((Math.random() * 10) + 1);
