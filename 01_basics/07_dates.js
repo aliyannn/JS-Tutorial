b917068a0bbf28c92b3d9300e541cf3e427eeaaa
Aliyan Gohar <78789032+aliyannn@users.noreply.github.com>
2024-01-31 15:30:17 +0000

    Date and Time in JS

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -9,5 +9,21 @@ let myDate = new Date();
 // console.log(`toLocaleTimeString = ` + myDate.toLocaleTimeString());
 // console.log(`type of Date = ` + typeof myDate);
 
-let myCreatedDate = new Date(2024, 0, 31, 17, 44);
-console.log(myCreatedDate.toLocaleString())
+// let myCreatedDate = new Date(2024, 0, 31, 17, 44);
+// console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDateString = new Date("01-31-2024");
+// console.log(myCreatedDateString.toLocaleDateString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+
+// console.log(myCreatedDateString.getTime());
+
+// console.log(Math.floor(Date.now() / 1000));
+
+// console.log(myCreatedDateString.getFullYear());
+// console.log(myCreatedDateString.getMonth() + 1);
+// console.log(myCreatedDateString.getDate());
+// console.log(myCreatedDateString.getDay());
+// console.log(myCreatedDateString.getTime());
